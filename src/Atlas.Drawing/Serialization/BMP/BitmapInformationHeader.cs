26d65268109a12e51f64b5e4970dafb5f13808ec
Daniel Cirket <daniel@atlascode.com>
2016-11-30 13:42:58 +0000

    Cleanup BitmapInformationHeader

--- src/Atlas.Drawing/Serialization/BMP/BitmapInformationHeader.cs
+++ src/Atlas.Drawing/Serialization/BMP/BitmapInformationHeader.cs
@@ -1,29 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Atlas.Drawing.Serialization.BMP
 {
     public class BitmapInformationHeader
     {
-        public UInt32 DataLength { get { return HeaderSize; } }
-        public UInt32 HeaderSize { get; set; }
-        public Int32 Width { get; set; }
-        public Int32 Height { get; set; }
-        public UInt16 Planes { get; set; }
-        public UInt16 BitsPerPixel { get; set; }
-        public UInt32 Compression { get; set; }
-        public UInt32 ImageSize { get; set; }
-        public UInt32 XPixelsPerMeter { get; set; }
-        public UInt32 YPixelsPerMeter { get; set; }
-        public UInt32 ColorsInColorTable { get; set; }
-        public UInt32 ImportantColorCount { get; set; }
-        public Int32 RedChannelBitmask { get; set; }
-        public Int32 GreenChannelBitmask { get; set; }
-        public Int32 BlueChannelBitmask { get; set; }
-        public Int32 AlphaChannelBitmask { get; set; }
-
+        public uint DataLength => HeaderSize;
+        public uint HeaderSize { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public ushort Planes { get; set; }
+        public ushort BitsPerPixel { get; set; }
+        public uint Compression { get; set; }
+        public uint ImageSize { get; set; }
+        public uint XPixelsPerMeter { get; set; }
+        public uint YPixelsPerMeter { get; set; }
+        public uint ColorsInColorTable { get; set; }
+        public uint ImportantColorCount { get; set; }
+        public int RedChannelBitmask { get; set; }
+        public int GreenChannelBitmask { get; set; }
+        public int BlueChannelBitmask { get; set; }
+        public int AlphaChannelBitmask { get; set; }
         public bool SupportsAlpha { get; private set; }
 
         public void Deserialize(byte[] bytes)
@@ -43,12 +39,14 @@ namespace Atlas.Drawing.Serialization.BMP
             Width = BitConverter.ToInt32(bytes, 14 + headerPosition); headerPosition += 4;
             Height = BitConverter.ToInt32(bytes, 14 + headerPosition); headerPosition += 4;
 
-            if (headerPosition >= HeaderSize) return;
+            if (headerPosition >= HeaderSize)
+                return;
 
             Planes = BitConverter.ToUInt16(bytes, 14 + headerPosition); headerPosition += 2;
             BitsPerPixel = BitConverter.ToUInt16(bytes, 14 + headerPosition); headerPosition += 2;
 
-            if (headerPosition >= HeaderSize) return;
+            if (headerPosition >= HeaderSize)
+                return;
 
             Compression = BitConverter.ToUInt32(bytes, 14 + headerPosition); headerPosition += 4;
             ImageSize = BitConverter.ToUInt32(bytes, 14 + headerPosition); headerPosition += 4;
@@ -57,18 +55,21 @@ namespace Atlas.Drawing.Serialization.BMP
             ColorsInColorTable = BitConverter.ToUInt32(bytes, 14 + headerPosition); headerPosition += 4;
             ImportantColorCount = BitConverter.ToUInt32(bytes, 14 + headerPosition); headerPosition += 4;
 
-            if (headerPosition >= HeaderSize && Compression != 3 && Compression != 6) return;
+            if (headerPosition >= HeaderSize && Compression != 3 && Compression != 6)
+                return;
 
             RedChannelBitmask = BitConverter.ToInt32(bytes, 14 + headerPosition); headerPosition += 4;
             GreenChannelBitmask = BitConverter.ToInt32(bytes, 14 + headerPosition); headerPosition += 4;
             BlueChannelBitmask = BitConverter.ToInt32(bytes, 14 + headerPosition); headerPosition += 4;
 
-            if (headerPosition >= HeaderSize && Compression != 6) return;
+            if (headerPosition >= HeaderSize && Compression != 6)
+                return;
 
             AlphaChannelBitmask = BitConverter.ToInt32(bytes, 14 + headerPosition); headerPosition += 4;
             SupportsAlpha = AlphaChannelBitmask != 0;
 
-            if (headerPosition >= HeaderSize) return;
+            if (headerPosition >= HeaderSize)
+                return;
 
             // ColourSpaceType
             //ColourSpaceEndpoints
