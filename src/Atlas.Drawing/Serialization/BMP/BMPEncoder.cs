5d85177d2e97d2492d5f4fa5db565c3f0184f735
Daniel Cirket <daniel@atlascode.com>
2016-11-30 13:45:28 +0000

    Cleanup BMPEncoder

--- src/Atlas.Drawing/Serialization/BMP/BMPEncoder.cs
+++ src/Atlas.Drawing/Serialization/BMP/BMPEncoder.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Atlas.Drawing.Serialization.BMP
+﻿namespace Atlas.Drawing.Serialization.BMP
 {
     public class BMPEncoder
     {
