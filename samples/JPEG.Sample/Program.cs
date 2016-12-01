cb0bdb4a2400834be0d1fe3e28439b4ca3853863
Daniel Cirket <daniel@atlascode.com>
2016-12-01 13:10:20 +0000

    Add JPEG sample project in preparation for jpeg decoder

---
+++ samples/JPEG.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Atlas.Drawing;
+using Atlas.Drawing.Imaging;
+
+namespace JPEG.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Directory.CreateDirectory("output");
+
+            foreach (var file in Directory.GetFiles(@"samples", "*.jpg"))
+            {
+                var jpg = Image.FromFile(file);
+                jpg.Save(Path.Combine("output", Path.GetFileNameWithoutExtension(file) + ".png"), ImageFormat.Png);
+            }
+        }
+    }
+}
