0a2a4fc9bf21d0255a96e69c86d122e94c48cb10
Daniel Cirket <daniel@atlascode.com>
2016-12-07 09:04:17 +0000

    Added imageformat exception for corrupt/malformed images

---
+++ src/Atlas.Drawing/ImageFormatException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Atlas.Drawing
+{
+    internal class ImageFormatException : Exception
+    {
+        public ImageFormatException() { }
+        public ImageFormatException(string message) : base(message) { }
+        public ImageFormatException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
